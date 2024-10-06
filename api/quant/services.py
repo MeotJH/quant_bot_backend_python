c7831cd64b2511ad232d77700aae77c437701351
Meotjh <marionette934@gmail.com>
2024-10-06 20:01:16 +0900

    ADD 유저 프로필별 퀀트리스트 API

--- api/quant/services.py
+++ api/quant/services.py
@@ -1,9 +1,12 @@
+from numbers import Number
+
 import yfinance as yf
 from flask_jwt_extended import get_jwt_identity
 
 from api import db
 from api.quant.entityies import Quant
 from api.user.entities import User
+from exceptions import BadRequestException
 
 
 def find_stock_by_id(item_id, period='1y', trend_follow_days=75):
@@ -68,4 +71,38 @@ def register_quant_by_stock(stock: str, quant_type: str):
         return dict
     except Exception as e:
         db.session.rollback()
-        return {"error": f"Failed to save Quant: {str(e)}"}
+        raise BadRequestException(f'{e}', 400)  # 비밀번호가 틀렸을 때 예외 발
+
+def find_quants_by_user():
+    jwt_user = get_jwt_identity()
+    user = User.query.filter_by(email=jwt_user).first()
+    quants = Quant.query.filter_by(user_id=user.uuid).all()
+
+
+    quants_dict = []
+    for quant in quants:
+        stock_id = quant.stock
+        stock = find_stock_by_id(stock_id)
+        recent_stock = stock["stock_history"][0]
+
+        # 모델에서 가져온 값을 가정
+        previous_close = float(recent_stock['Prev_Close'])  # 모델에서 previousClose 값을 가져옴
+        last_cross_trend_follow = float(recent_stock['Prev_Trend_Follow'])  # 모델에서 lastCrossTrendFollow 값을 가져옴
+
+        # 수익 및 수익률 계산
+        profit = previous_close - last_cross_trend_follow
+        profit_percent = (profit / previous_close) * 100
+
+        # 결과 출력
+        print(f"Profit: {profit}")
+        print(f"Profit Percentage: {profit_percent}%")
+        quant_one = {
+            "stock": stock_id,
+            "profit": round(profit, 2),
+            "profit_percent":  round(profit_percent, 2),
+            "notification" : quant.notification,
+            "quant_type" : quant.quant_type,
+        }
+        quants_dict.append(quant_one)
+
+    return quants_dict
