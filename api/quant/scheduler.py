e746fb32b95745a45e1e8c10f04bcfdc3ea8a6bf
Meotjh <marionette934@gmail.com>
2024-10-20 12:07:38 +0900

    FIX 스케줄러 config 에러

--- api/quant/scheduler.py
+++ api/quant/scheduler.py
@@ -1,14 +1,31 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from api.quant.services import QuantService
+import logging
+from apscheduler.schedulers.base import SchedulerNotRunningError
+
+logger = logging.getLogger(__name__)
 
 class QuantScheduler:
-    def __init__(self):
-        self.scheduler = BackgroundScheduler()
-        self.quant_service = QuantService()
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(QuantScheduler, cls).__new__(cls)
+            cls._instance.scheduler = BackgroundScheduler()
+            cls._instance.quant_service = QuantService()
+        return cls._instance
 
     def start(self):
-        self.scheduler.add_job(self.quant_service.check_and_notify, 'interval', minutes=30)
-        self.scheduler.start()
+        if not self.scheduler.running:
+            self.scheduler.add_job(self.quant_service.check_and_notify, 'interval', minutes=30)
+            self.scheduler.start()
+            logger.info("Quant Scheduler started")
+        else:
+            logger.info("Quant Scheduler is already running")
 
     def shutdown(self):
-        self.scheduler.shutdown()
+        if self.scheduler.running:
+            logger.info("Shutting down Quant Scheduler")
+            self.scheduler.shutdown()
+        else:
+            logger.warning("Quant Scheduler is not running")
