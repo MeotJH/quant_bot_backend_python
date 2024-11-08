5878b078699c1036539c9cfeb68a8909f34c0915
MeotJH <66897806+MeotJH@users.noreply.github.com>
2024-11-08 21:03:29 +0900

    HOTFIX 스케줄러 시간 설정 에러 (#15)
    * 추세추종 알림 디테일 기능
    
    * FIX yfinance 함수 리팩토링 에러 제거
    
    * ADD 단위테스트 스케줄링
    
    * ADD 알림기능 마무리
    
    * 에러 수정
    
    * FIX 퀀트 알림 에러
    
    * FIX 배치 스케줄러 cronTrigger 시간 변경
    
    ---------
    
    Co-authored-by: Meotjh <marionette934@gmail.com>

--- api/quant/scheduler.py
+++ api/quant/scheduler.py
@@ -25,8 +25,8 @@ class QuantScheduler:
             # 한국 시간 밤 10:30과 11:30에 실행
             self.scheduler.add_job(
                 self._run_check_and_notify, 
-                #trigger=CronTrigger(hour='22', minute=30),
-                trigger=IntervalTrigger(seconds=10),
+                trigger=CronTrigger(hour='22', minute=30),
+                #trigger=IntervalTrigger(seconds=10),
                 timezone=timezone('Asia/Seoul')
             )
             self.scheduler.start()
