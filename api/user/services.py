699ddc8d809cde5cc89966b80df72a8d361b71b5
Meotjh <marionette934@gmail.com>
2024-11-09 23:23:13 +0900

    앱토큰 안받았을때 예외처리 추가

--- api/user/services.py
+++ api/user/services.py
@@ -8,7 +8,13 @@ from flask_jwt_extended import create_access_token, get_jwt_identity
 def save_user(user):
     id = uuid4()
     password_hash = generate_password_hash(user['password'])
-    new_user = User(uuid=id, username=user['userName'], email=user['email'], password=password_hash, app_token=user['appToken'])
+
+    if user['appToken'] is None:
+        app_token = ''
+    else:
+        app_token = user['appToken']
+
+    new_user = User(uuid=id, username=user['userName'], email=user['email'], password=password_hash, app_token=app_token)
     db.session.add(new_user)
     db.session.commit()
     return new_user.to_dict()
